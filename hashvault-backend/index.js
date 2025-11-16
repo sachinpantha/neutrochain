8e7aff765e26df093feed7bf27684967296f7cda
Sachiiiiin <sachinpantha69@gmail.com>
2025-11-16 10:56:30 +0545

    indexjs fixed

--- hashvault-backend/index.js
+++ hashvault-backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const neutroChainRouter = require('./neutrochain');
+const neutroChainRouter = require('./src/neutrochain');
 app.use('/api/neutrochain', neutroChainRouter);
 
 const PORT = process.env.PORT || 5000;
